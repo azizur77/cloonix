b801aa8dce8f918d19929e320c26cce33e1d29dd
perrier <perrier@debian>
2016-12-04 01:48:11 +0100

    Regression: wireshark was called through dropbear ssh with the -t which was wrong. Previous correction of the -t option revealed this bug

--- cloonix/client/cairo_canvas/src/menu_utils.c
+++ cloonix/client/cairo_canvas/src/menu_utils.c
@@ -391,8 +391,7 @@ void start_wireshark(char *name, t_bank_item *bitem)
   char *doors = get_doors_client_addr();
   char *password = get_password();
   char *recpath = bitem->pbi.pbi_sat->snf_info.recpath;
-  char *argv[] = {bin_path, doors, password,
-                  "-t", "local_host_dropbear", cmd, NULL}; 
+  char *argv[] = {bin_path, doors, password, "local_host_dropbear", cmd, NULL}; 
   memset(bin_path, 0, MAX_PATH_LEN);
   memset(cmd, 0, 2*MAX_PATH_LEN);
   snprintf(bin_path,  MAX_PATH_LEN-1,
