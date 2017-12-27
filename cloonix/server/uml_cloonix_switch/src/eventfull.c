c19d30a2a97941ab432c08f9da40cd8c348156ea
perrier <clownix@clownix.net>
2017-12-27 19:49:10 +0100

    bad previous correction, new correction

--- cloonix/server/uml_cloonix_switch/src/eventfull.c
+++ cloonix/server/uml_cloonix_switch/src/eventfull.c
@@ -254,34 +254,31 @@ static int collect(t_eventfull_endp *eventfull, int nb, t_endp *endp)
 {
   int i, real_nb = 0;
   t_endp *next, *cur = endp;
-  t_vm *vm;
+  t_vm *vm = NULL;
   for (i=0; i<nb; i++)
     {
     if (!cur)
       KOUT(" ");
     if (strlen(cur->name) == 0)
       KERR("%d", cur->endp_type);
-    else
+    else if (!((cur->endp_type == endp_type_kvm) && (!cfg_get_vm(cur->name))))
       {
-      vm = cfg_get_vm(cur->name);
-      if (vm)
+      strncpy(eventfull[i].name, cur->name, MAX_NAME_LEN-1);
+      eventfull[i].num  = cur->num;
+      eventfull[i].type = cur->endp_type;
+      if ((cur->endp_type == endp_type_kvm) && (cur->num == 0)) 
         {
-        strncpy(eventfull[i].name, cur->name, MAX_NAME_LEN-1);
-        eventfull[i].num  = cur->num;
-        eventfull[i].type = cur->endp_type;
-        if ((cur->endp_type == endp_type_kvm) && (cur->num == 0)) 
-          {
-          eventfull[i].ram  = vm->ram;
-          eventfull[i].cpu  = vm->cpu;
-          }
-        eventfull[i].ok   = cur->c2c.is_peered;
-        eventfull[i].ptx  = add_all_tidx_ptx(cur->lan_attached);
-        eventfull[i].prx  = add_all_tidx_prx(cur->lan_attached);
-        eventfull[i].btx  = add_all_tidx_btx(cur->lan_attached);
-        eventfull[i].brx  = add_all_tidx_brx(cur->lan_attached);
-        eventfull[i].ms   = get_last_ms(cur->lan_attached);
-        real_nb += 1;
+        vm = cfg_get_vm(cur->name);
+        eventfull[i].ram  = vm->ram;
+        eventfull[i].cpu  = vm->cpu;
         }
+      eventfull[i].ok   = cur->c2c.is_peered;
+      eventfull[i].ptx  = add_all_tidx_ptx(cur->lan_attached);
+      eventfull[i].prx  = add_all_tidx_prx(cur->lan_attached);
+      eventfull[i].btx  = add_all_tidx_btx(cur->lan_attached);
+      eventfull[i].brx  = add_all_tidx_brx(cur->lan_attached);
+      eventfull[i].ms   = get_last_ms(cur->lan_attached);
+      real_nb += 1;
       }
     next = endp_mngt_get_next(cur);
     clownix_free(cur, __FILE__);
